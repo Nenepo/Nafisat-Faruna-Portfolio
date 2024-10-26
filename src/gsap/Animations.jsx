494e795cd4d564974ec3cb86a15d8aed3c9e1ea5
Nafisa <Nafisafaruna@gmail.com>
2024-10-26 20:44:11 +0100

    updated ui

--- src/gsap/Animations.jsx
+++ src/gsap/Animations.jsx
@@ -10,7 +10,7 @@ gsap.registerPlugin(ScrollTrigger);
       scrollTrigger: {
         trigger: element,
         start: "top 80%",
-        end: "top 60%",
+        // end: "top 60%",
         scrub: true,
       },
       opacity: 1,
@@ -24,7 +24,7 @@ gsap.registerPlugin(ScrollTrigger);
       scrollTrigger: {
         trigger: element1,
         start: "top 80%",
-        end: "top 60%",
+        // end: "top 60%",
         scrub: true,
         onEnter: () => gsap.to(element2, { opacity: 0, y: -20, duration: 1, ease: "power1.out" }),
         onEnterBack: () => gsap.to(element2, { opacity: 1, y: 0, duration: 1, ease: "power1.in" }),
--- src/work.js
+++ src/work.js
@@ -10,14 +10,5 @@ export const works = [
     // githubRepo: 'https://github.com/user/photoruum-join-form'
   },
 
-  {
-    id: 2,
-    title: 'The Photoruum Join Form',
-    img: '/projectImages/photoruum1.png', 
-    description: 'A community dedicated to promoting and celebrating photographers in Africa.',
-    text: 'The Photoruum Join Form is a multi-step form designed to streamline the onboarding process for photographers joining the community. It guides users through steps including providing personal information, portfolio samples, and social media links, ensuring that only authentic members are registered. The form also utilizes real-time validation and responsive design for a seamless experience across devices.',
-    link: 'https://www.photoruum.com/join',
-    techUsed: ['React', 'TailwindCss', 'Typescript', 'Swiperjs'],
-    // githubRepo: 'https://github.com/user/photoruum-join-form'
-  },
+  
 ];
