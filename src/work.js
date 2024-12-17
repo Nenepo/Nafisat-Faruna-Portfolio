37b0cfeedeea86dd9cfe7c1a651d28e8cc404f2b
Nafisa <Nafisafaruna@gmail.com>
2024-12-17 14:44:56 +0100

    update

--- src/work.js
+++ src/work.js
@@ -27,15 +27,24 @@ export const works = [
     text: 'The Bento Grid is a responsive, CSS-based grid layout that arranges content in a visually appealing and organized manner, inspired by the structured compartments of a bento box. It’s ideal for showcasing items like images, product features, or articles, providing a clean, modern aesthetic. The grid adapts fluidly to various screen sizes, ensuring content is well-organized and accessible across devices. Users can customize grid items to highlight different elements, making it an excellent solution for portfolios, galleries, and e-commerce layouts.',
     techUsed: ['css grid', 'HTML' ],
   },
-  // {
-  //   id: 4,
-  //   title: 'The Stucruum Landing Page',
-  //   img: '/projectImages/stucruum.png', 
-  //   description: 'A community dedicated to promoting and celebrating photographers in Africa.',
-  //   text: 'The Photoruum Join Form is a multi-step form designed to streamline the onboarding process for photographers joining the community. It guides users through steps including providing personal information, portfolio samples, and social media links, ensuring that only authentic members are registered. The form also utilizes real-time validation and responsive design for a seamless experience across devices.',
-  //   link: 'https://www.stucruum.com',
-  //   techUsed: ['React', 'TailwindCss','GSAP'],
-  //   // githubRepo: 'https://github.com/user/photoruum-join-form'
-  // },
+  {
+    id: 4,
+    title: 'The Stucruum Landing Page',
+    img: '/projectImages/stucruum.png', 
+    description: 'A community dedicated to promoting and celebrating photographers in Africa.',
+    text: 'The Stucruum is a solution for Afro stock images',
+    link: 'https://www.stucruum.com',
+    techUsed: ['React', 'TailwindCss','GSAP'],
+    // githubRepo: 'https://github.com/user/photoruum-join-form'
+  },
+  {
+    id: 5,
+    title: 'Android app testers',
+    img: '/projectImages/appTesters.png', 
+    description: 'A company dedicated to help app developers ship their apps on google playstore',
+    text: 'The Android App Testers website was created to connect app developers with skilled testers who ensure their applications are bug-free and optimized before launch. The platform showcases the company’s mission to support developers in delivering polished apps by providing user feedback, identifying issues, and improving overall app quality. The site is designed with responsive features, a smooth scrolling experience, and Firebase integration for dynamic content management.',
+    link: 'https://www.androidapptesters.com',
+    techUsed: ['React', 'TailwindCss','React-scroll', 'firebase'],
+  },
  
 ];
